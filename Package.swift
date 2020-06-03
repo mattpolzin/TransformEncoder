500bca081abd67ea6f8c6bca253419d80fad66b5
Mathew Polzin <matt.polzin@gmail.com>
2020-06-02 22:54:28 -0700

    remove unused Poly dependency

--- Package.swift
+++ Package.swift
@@ -9,14 +9,12 @@ let package = Package(
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "TransformEncoder",
             targets: ["TransformEncoder"]),
     ],
     dependencies: [
         .package(url: "https://github.com/mattpolzin/OrderedDictionary.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/mattpolzin/Poly.git", .upToNextMajor(from: "2.3.1")),
         .package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "2.0.0")) // only for tests
     ],
     targets: [
@@ -24,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TransformEncoder",
-            dependencies: ["Poly", "OrderedDictionary"]),
+            dependencies: ["OrderedDictionary"]),
         .testTarget(
             name: "TransformEncoderTests",
             dependencies: ["TransformEncoder", "Yams"]),
